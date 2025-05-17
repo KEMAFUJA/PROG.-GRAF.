cb406970c8b7b53a32250014f824cc25c535e9c0
KEMAFUJA <kevin_mfuentes98@hotmail.com>
2025-05-17 16:12:40 -0400

    Delete SerializableShape3D.cs

--- SerializableShape3D.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Numerics;
-
-public class SerializableShape3D
-{
-    public List<float> Vertices { get; set; }
-    public List<uint> Indices { get; set; }
-    public Matrix4x4 ModelMatrix { get; set; }
-}
